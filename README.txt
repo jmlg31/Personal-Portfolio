db73ba71663539af8ec8bd1168d402440785552c
jmlg31 <jml310190@gmail.com>
2016-02-26 18:39:03 +0100

    Portfolio Personal
    Portfolio Personal con mis proyectos y contacto.

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/jm_90/pen/RrBWVV.
+
+ Personal portfolio where I talk about my skills and knowledge and show my personal projects.
---
+++ js/index.js
@@ -0,0 +1,65 @@
+/*Menu*/
+
+$(function() {
+      var pull    = $('#pull');
+        menu    = $('nav ul');
+        menuHeight  = menu.height();
+
+      $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+      });
+
+      $(window).resize(function(){
+            var w = $(window).width();
+            if(w > 320 && menu.is(':hidden')) {
+              menu.removeAttr('style');
+            }
+        });
+    });
+
+
+/*Portfolio*/
+
+$(function () {
+
+    var filterList = {
+
+      init: function () {
+
+        // MixItUp plugin
+        // http://mixitup.io
+        $('#portfoliolist').mixitup({
+          targetSelector: '.portfolio',
+          filterSelector: '.filter',
+          effects: ['fade'],
+          easing: 'snap',
+          // call the hover effect
+          onMixEnd: filterList.hoverEffect()
+        });
+
+      },
+
+      hoverEffect: function () {
+
+        // Simple parallax effect
+        $('#portfoliolist .portfolio').hover(
+          function () {
+            $(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+            $(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');
+          },
+          function () {
+            $(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+            $(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');
+          }
+        );
+
+      }
+
+    };
+
+    // Run the show!
+    filterList.init();
+
+
+  });
